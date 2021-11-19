e1cab0e19badee0fcb280288d9b7d07670f43ee5
Cyber404 <90260593+Cyber404hackers@users.noreply.github.com>
2021-11-19 13:46:38 +0200

    Add files via upload

---
+++ FACEBOOK C.py
@@ -0,0 +1,87 @@
+friends="""
+ \033[4m^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\033[0m
+          ★BLACK HAT HACKER§★
+ ™Anonyteach        ;[\033[32monline tsp 78.2kb √\033[0m]
+ ™bestkind          :[\033[31monline tsp 99.0kb √\033[0m]
+ ™Vision            :[\033[32monline tsp 23.5kb √\033[0m]
+ ™CraZy KiD         :[\033[32monline tsp 50.6kb √\033[0m]
+ ™Empire Monster    :[\033[32monline tsp 02.0kb √\033[0m]
+ ™Hamroks           :[\033[31monline tsp 57.6kb √\033[0m]
+ ™De GeneRal SA     :[\033[32monline tsp 84.6kb √\033[0m]
+ ™Professor         :[\033[31monline tsp 52.6kb √\033[0m]
+ ™CraZy GoD         :[\033[32monline tsp 57.6kb √\033[0m]
+ ™Nailock           :[\033[32monline tsp 37.6kb √\033[0m]
+ 
+ 
+"""
+print(friends)
+num="""
+\033[31mcracking database \033[0m000000000000000\033[32m00\033[0m000000000000\033[32m00\033[0m000000000  0000000000000\033[31mcracking database\033[0m000\033[0m00000000000
+"""
+logo2="""
+\33[92m TTSP oftware installed\033[0m
+╠═══════════════════════════════════════════════════════════\033[0m
+\033[1m⊂_ ヽ
+　 ＼＼ Λ＿Λ
+　　 ＼( 'ㅅ' )
+　　　 >　⌒ヽ
+　　　/ 　 へ＼
+　　 /　　/　＼＼error    4
+　　 ﾚ　ノ　　 ヽ_つ=====➔     0	
+　　/　/                     ======➔       4
+　 /　/|                              =======➔
+　(　(ヽ
+　|　|、＼
+　| 丿 ＼ ⌒)
+　| |　　) /
+'ノ )　　Lﾉ.                       https://t.me/hamza_ayoola
+╠═══════════════════════════════════════════════════════════
+                \33[96m\033[4m√profesor william & BESTKIND√\033[0m
+"""
+opt="""
+[\033[31m01\033[0m]facebook         [\033[31m02\033[0m]whatappp     [\033[31m03\033[0m]telegram          
+"""
+opt2="""
+[\033[31m01\033[0m]facebook         [\033[31m02\033[0m]whatappp     [\033[31m03\033[0m]telegram
+          
+[\033[31m04\033[0m]virus            [\033[31m05\033[0m]Tiktok       [\033[31m07\033[0m]twitter
+
+[\033[31m08\033[0m]bank details     [\033[31m09\033[0m]wi-fi        [\033[31m10\033[0m]spam user
+
+[\033[31m11\033[0m]sni test         [\033[31m12\033[0m]Ban          [\033[31m13\033[0m]telegram 
+      
+[\033[31m14\033[0m]FB likes         [\033[31m15\033[0m]instagram    [\033[31m16\033[0m]networking
+"""
+opt5="""
+[\033[31m01\033[0m]sni test         [\033[31m02\033[0m]Ban     [\033[31m03\033[0m]telegram          
+"""
+opt3="""
+\033[42m[00]more
+[0#]contact us
+\033[0m
+"""
+p=str()
+usll=str()
+while usll !="@bestkind123b79":
+	      usll =input("username:")
+	      if  usll !="@bestkind123":  	
+   	       p=input("password:")
+print("\n                    \033[91maccess granted\033[0m")
+	     	      
+print(logo2)
+print(opt2)
+print(opt3)
+opt=input("==›")
+#hard to tell
+if opt=="01":
+	print("engine processing..."*12)
+else:
+	print("other options are not working for now please use (01)")
+while True:
+  if opt=="01":
+   print("       facebook hacking... ")
+   v=input("\033[4mFB user»\033[0m")
+   break
+print(num*3000)
+print("⁝⁞⁝⁞⁝⁞⁝⁞⁝⁞⁝⁞⁝⁞⁝⁞⁝⁞⁝⁞⁝⁞⁝⁞⁝⁞⁝⁞⁝⁞⁝⁞⁝⁞⁝⁞⁝⁞⁝⁞⁝⁞⁝⁞⁝⁞⁝⁞⁝⁞⁝⁞⁝⁞⁝⁞⁝⁞⁝⁞⁝⁞⁝⁞⁝⁞⁝⁞⁝⁞⁝⁞⁝⁞⁝⁞⁝⁞⁝⁞⁝⁞⁝⁞⁝⁞⁝⁞⁝⁞⁝⁞⁝⁞⁝⁞⁝⁞⁝⁞⁝⁞⁝⁞⁝⁞⁝⁞⁝")
+print("\n\n<found "+v+">\n<found \033[32msttp\033[0m>\n<found " +v+">\n\n\nusername:"+v+"\npassword:\033[31mtakalane\033[0m")
